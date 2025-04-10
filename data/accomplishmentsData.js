de1d64d4f9ff04562a920629bd6c2b093c2fd146
John Philip <johnphilipomamalin1206@gmail.com>
2025-04-10 23:24:44 +0800

    added accomplishments pictures and page modified

--- data/accomplishmentsData.js
+++ data/accomplishmentsData.js
@@ -1,50 +1,206 @@
 // data/accomplishmentsData.js
 import { MdWork, MdSchool, MdStar } from "react-icons/md";
+import { GrRobot } from "react-icons/gr";
 
 const accomplishmentsData = [
   {
-    date: "2019",
-    cardTitle: "Internet-of-Things: Monitoring and Controlling Electrical Appliances",
-    image:
-      "https://jphlpl-portfolio-bucket.s3.ap-southeast-1.amazonaws.com/accomplishments/Thesis2.jpg",
-    cardDetailedText:
-      "Our final capstone project that promotes an efficient and remote way of managing electrical appliances.",
-    icon: MdWork,
-    iconStyle: { background: "rgb(59, 130, 246)", color: "#fff" }, // Tailwind blue-500
+    date: "2024",
+    cardTitle: "Lorem Ipsum",
+    image: "/images/accomplishments/accomplishment15.jpg",
+    cardDetailedText: "Lorem Ipsum",
+    icon: MdStar,
+    iconStyle: { background: "#DAB060", color: "#fff" }, // Tailwind pink-500
     contentStyle: {
       background: "#f3f4f6", // Tailwind gray-100
-      color: "#1f2937"       // Tailwind gray-800
+      color: "#1f2937", // Tailwind gray-800
     },
-    contentArrowStyle: { borderRight: "7px solid #f3f4f6" }
+    contentArrowStyle: { borderRight: "7px solid #fef3c7" },
+  },
+  {
+    date: "2024",
+    cardTitle: "Lorem Ipsum",
+    image: "/images/accomplishments/accomplishment14.jpg",
+    cardDetailedText: "Lorem Ipsum",
+    icon: MdWork,
+    iconStyle: { background: "rgb(59, 130, 246)", color: "#fff" }, // Tailwind pink-500
+    contentStyle: {
+      background: "#f3f4f6", // Tailwind gray-100
+      color: "#1f2937", // Tailwind gray-800
+    },
+    contentArrowStyle: { borderRight: "7px solid #fef3c7" },
+  },
+  {
+    date: "2024",
+    cardTitle: "Lorem Ipsum",
+    image: "/images/accomplishments/accomplishment13.jpg",
+    cardDetailedText: "Lorem Ipsum",
+    icon: MdWork,
+    iconStyle: { background: "rgb(59, 130, 246)", color: "#fff" }, // Tailwind pink-500
+    contentStyle: {
+      background: "#f3f4f6", // Tailwind gray-100
+      color: "#1f2937", // Tailwind gray-800
+    },
+    contentArrowStyle: { borderRight: "7px solid #fef3c7" },
+  },
+  {
+    date: "2022",
+    cardTitle: "Lorem Ipsum",
+    image: "/images/accomplishments/accomplishment12.jpg",
+    cardDetailedText: "Lorem Ipsum",
+    icon: GrRobot,
+    iconStyle: { background: "#D5F199", color: "#fff" }, // Tailwind pink-500
+    contentStyle: {
+      background: "#f3f4f6", // Tailwind gray-100
+      color: "#1f2937", // Tailwind gray-800
+    },
+    contentArrowStyle: { borderRight: "7px solid #fef3c7" },
+  },
+  {
+    date: "2022",
+    cardTitle: "Lorem Ipsum",
+    image: "/images/accomplishments/accomplishment11.jpg",
+    cardDetailedText: "Lorem Ipsum",
+    icon: GrRobot,
+    iconStyle: { background: "#D5F199", color: "#fff" },  // Tailwind pink-500
+    contentStyle: {
+      background: "#f3f4f6", // Tailwind gray-100
+      color: "#1f2937", // Tailwind gray-800
+    },
+    contentArrowStyle: { borderRight: "7px solid #fef3c7" },
+  },
+  {
+    date: "2022",
+    cardTitle: "Lorem Ipsum",
+    image: "/images/accomplishments/accomplishment10.jpg",
+    cardDetailedText: "Lorem Ipsum",
+    icon: MdWork,
+    iconStyle: { background: "rgb(59, 130, 246)", color: "#fff" }, // Tailwind pink-500
+    contentStyle: {
+      background: "#f3f4f6", // Tailwind gray-100
+      color: "#1f2937", // Tailwind gray-800
+    },
+    contentArrowStyle: { borderRight: "7px solid #fef3c7" },
+  },
+  {
+    date: "2022",
+    cardTitle: "Lorem Ipsum",
+    image: "/images/accomplishments/accomplishment9.jpg",
+    cardDetailedText: "Lorem Ipsum",
+    icon: GrRobot,
+    iconStyle: { background: "#D5F199", color: "#fff" }, 
+    contentStyle: {
+      background: "#f3f4f6", // Tailwind gray-100
+      color: "#1f2937", // Tailwind gray-800
+    },
+    contentArrowStyle: { borderRight: "7px solid #fef3c7" },
+  },
+  {
+    date: "2022",
+    cardTitle: "Lorem Ipsum",
+    image: "/images/accomplishments/accomplishment8.jpg",
+    cardDetailedText: "Lorem Ipsum",
+    icon: GrRobot,
+    iconStyle: { background: "#D5F199", color: "#fff" }, 
+    contentStyle: {
+      background: "#f3f4f6", // Tailwind gray-100
+      color: "#1f2937", // Tailwind gray-800
+    },
+    contentArrowStyle: { borderRight: "7px solid #fef3c7" },
+  },
+  {
+    date: "2022",
+    cardTitle: "Lorem Ipsum",
+    image: "/images/accomplishments/accomplishment7.jpg",
+    cardDetailedText: "Lorem Ipsum",
+    icon: MdWork,
+    iconStyle: { background: "rgb(59, 130, 246)", color: "#fff" }, // Tailwind pink-500
+    contentStyle: {
+      background: "#f3f4f6", // Tailwind gray-100
+      color: "#1f2937", // Tailwind gray-800
+    },
+    contentArrowStyle: { borderRight: "7px solid #fef3c7" },
+  },
+  {
+    date: "2022",
+    cardTitle: "Lorem Ipsum",
+    image: "/images/accomplishments/accomplishment6.jpg",
+    cardDetailedText: "Lorem Ipsum",
+    icon: MdWork,
+    iconStyle: { background: "rgb(59, 130, 246)", color: "#fff" }, // Tailwind pink-500
+    contentStyle: {
+      background: "#f3f4f6", // Tailwind gray-100
+      color: "#1f2937", // Tailwind gray-800
+    },
+    contentArrowStyle: { borderRight: "7px solid #fef3c7" },
+  },
+  {
+    date: "2022",
+    cardTitle: "Lorem Ipsum",
+    image: "/images/accomplishments/accomplishment5.jpg",
+    cardDetailedText: "Lorem Ipsum",
+    icon: MdWork,
+    iconStyle: { background: "rgb(59, 130, 246)", color: "#fff" }, // Tailwind pink-500
+    contentStyle: {
+      background: "#f3f4f6", // Tailwind gray-100
+      color: "#1f2937", // Tailwind gray-800
+    },
+    contentArrowStyle: { borderRight: "7px solid #fef3c7" },
+  },
+  {
+    date: "2022",
+    cardTitle: "Lorem Ipsum",
+    image: "/images/accomplishments/accomplishment4.jpg",
+    cardDetailedText: "Lorem Ipsum",
+    icon: MdWork,
+    iconStyle: { background: "rgb(59, 130, 246)", color: "#fff" }, // Tailwind pink-500
+    contentStyle: {
+      background: "#f3f4f6", // Tailwind gray-100
+      color: "#1f2937", // Tailwind gray-800
+    },
+    contentArrowStyle: { borderRight: "7px solid #fef3c7" },
+  },
+  {
+    date: "2022",
+    cardTitle: "Lorem Ipsum",
+    image: "/images/accomplishments/accomplishment3.jpg",
+    cardDetailedText: "Lorem Ipsum",
+    icon: MdWork,
+    iconStyle: { background: "rgb(59, 130, 246)", color: "#fff" }, // Tailwind pink-500
+    contentStyle: {
+      background: "#f3f4f6", // Tailwind gray-100
+      color: "#1f2937", // Tailwind gray-800
+    },
+    contentArrowStyle: { borderRight: "7px solid #fef3c7" },
+  },
+  {
+    date: "2019",
+    cardTitle:
+      "Internet-of-Things: Monitoring and Controlling Electrical Appliances",
+    image: "/images/accomplishments/accomplishment2.jpg",
+    cardDetailedText:
+      "Our final capstone project that promotes an efficient and remote way of managing electrical appliances.",
+    icon: MdSchool,
+    iconStyle: { background: "rgb(236, 72, 153)", color: "#fff" }, // Tailwind blue-500
+    contentStyle: {
+      background: "#f3f4f6", // Tailwind gray-100
+      color: "#1f2937", // Tailwind gray-800
+    },
+    contentArrowStyle: { borderRight: "7px solid #f3f4f6" },
   },
   {
     date: "2018",
     cardTitle: "Safety Driving Quiz Game App",
-    image:
-      "https://jphlpl-portfolio-bucket.s3.ap-southeast-1.amazonaws.com/accomplishments/Thesis1.jpg",
+    image: "/images/accomplishments/accomplishment1.jpg",
     cardDetailedText:
       "Our capstone project in Software Engineering Subject that I solely developed using Unity Engine C# with some Blender Assets.",
     icon: MdSchool,
     iconStyle: { background: "rgb(236, 72, 153)", color: "#fff" }, // Tailwind pink-500
     contentStyle: {
       background: "#f3f4f6", // Tailwind gray-100
-      color: "#1f2937"       // Tailwind gray-800
+      color: "#1f2937", // Tailwind gray-800
     },
-    contentArrowStyle: { borderRight: "7px solid #fef3c7" }
+    contentArrowStyle: { borderRight: "7px solid #fef3c7" },
   },
-  {
-    date: "2017",
-    cardTitle: "Special Recognition",
-    image: "https://example.com/your-image.jpg",
-    cardDetailedText: "Special recognition for outstanding contribution.",
-    icon: MdStar,
-    iconStyle: { background: "rgb(34, 197, 94)", color: "#fff" }, // Tailwind green-500
-    contentStyle: {
-      background: "#f3f4f6", // Tailwind gray-100
-      color: "#1f2937"       // Tailwind gray-800
-    },
-    contentArrowStyle: { borderRight: "7px solid #e0f2fe" }
-  }
 ];
 
 export default accomplishmentsData;
